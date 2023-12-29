696db8fa024d9354dfd99313c4396116c0affa61
dizzzackl <154916956+dizzzackl@users.noreply.github.com>
2023-12-29 13:04:57 +0300

    Add files via upload

---
+++ static.js
@@ -0,0 +1,12 @@
+<!-- Подключение скрипта Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=6501679242"></script>
+
+<!-- Инициализация и отправка данных в Google Analytics -->
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  // Задайте ваш ID Google Analytics вместо 'ТУТ_ВАШ_ИД' для обновления счетчиков.
+  gtag('config', '6501679242');
+</script>
